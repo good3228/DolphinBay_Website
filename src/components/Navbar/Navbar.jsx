40d7dbb87063b57758c0211d0a68ca747938d43c
good3228 <jim19980815@gmail.com>
2022-12-02 19:49:26 -0500

    navbar with styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -30,29 +30,46 @@ const Navbar = () => {
         </li>
       </ul>
       <div className="app__navbar-smallscreen">
-        <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {setToggleMenu(true)}} />
-        {toggleMenu &&
-          <div className="app__navbar-smallscreen_overlay flex_center slide_bottome">
+        <GiHamburgerMenu
+          color="#fff"
+          fontSize={27}
+          onClick={() => {
+            setToggleMenu(true);
+          }}
+        />
+        {toggleMenu && (
+          <div className="app__navbar-smallscreen_overlay flex__center slide_bottom">
             <MdOutlineRestaurantMenu
               fontSize={27}
-              className="overlay_close"
-              onClick={() => { setToggleMenu(false) }}
+              className="overlay__close"
+              onClick={() => {
+                setToggleMenu(false);
+              }}
             />
-            <ul className="app__navbar-links">
+            <ul className="app__navbar-smallscreen_links">
               <li className="p__opensans">
-                <a href="#home">Home</a>
+                <a href="#home" onClick={() => setToggleMenu(false)}>
+                  Home
+                </a>
               </li>
               <li className="p__opensans">
-                <a href="#about">About</a>
+                <a href="#about" onClick={() => setToggleMenu(false)}>
+                  About
+                </a>
               </li>
               <li className="p__opensans">
-                <a href="#Menu">Menu</a>
+                <a href="#Menu" onClick={() => setToggleMenu(false)}>
+                  Menu
+                </a>
               </li>
               <li className="p__opensans">
-                <a href="#contact">Contact</a>
+                <a href="#contact" onClick={() => setToggleMenu(false)}>
+                  Contact
+                </a>
               </li>
             </ul>
-          </div>}
+          </div>
+        )}
       </div>
     </nav>
   );
